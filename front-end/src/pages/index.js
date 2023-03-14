131652995fc57f8d838a110d27ad9fe4278be7dc
potato <jaafariramadan@gmail.com>
2023-03-14 15:53:40 +0200

    Initialized front-end Next JS app & created a deployment and a service for it

---
+++ front-end/src/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Bird Squawk</title>
+        <meta name="description" content="My first microservice Fullstack app!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>
+          Hallo Haw are you doing?
+        </h1>
+      </main>
+    </>
+  )
+}
